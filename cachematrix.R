19b3f4b4c08227c9cbf53bdecb817b4847e51407
Survesh Chauhan <surveshchauhan@live.com>
2014-12-22 02:46:24 +0530

    Adding comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,5 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## This function is used to pass a vector, this vector creates an object that stores vecor value and cached value. 
+##It is initiallyset to null
 
 makeCacheMatrix <- function(x = matrix()) {
 	i <- NULL
@@ -9,9 +7,21 @@ makeCacheMatrix <- function(x = matrix()) {
                 x <<- y
                 i <<- NULL
         }
-        get <- function() x
-        setinverse <- function(inverse) i <<- solve(x)
-        getinverse <- function() i
+	#This fucntion returns the value of original vector
+        get <- function() 
+	{
+		x
+	}
+	##setter method, called very first time from cacheSolve, subsequent calls with same vector will not be calling this 
+        setinverse <- function(inverse) 
+	{
+		i <<- solve(x)
+	}
+	##Called after first call returns cached value
+        getinverse <- function()
+	{	
+		 i
+	}
         list(set = set, get = get,
              setinverse = setinverse ,
              getinverse = getinverse )
@@ -19,15 +29,21 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this funCaction
+
+## When called gets the inverse  from the object x$getinverse (),
+## If value present message getting cached data will be printed
+## Otherwise inverse is done and displayed
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
-	i <- x$getinverse ()
+	i <- x$getinverse () #gets the value
+	## If not null gets cached value
+	
         if(!is.null(i)) {
                 message("getting cached data")
                 return(i)
         }
+	## Calcutale the inverse of matrix
         data <- x$get()
         i <- solve(data, ...)
         x$setinverse(i)
